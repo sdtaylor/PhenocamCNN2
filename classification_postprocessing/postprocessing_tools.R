13e55f056df5c31d1d1816d75ef369b82cc0b58f
Shawn <shawntaylor@weecology.org>
2021-05-05 17:12:57 -0600

    add sequence identification
    this is for HMM processing

--- classification_postprocessing/postprocessing_tools.R
+++ classification_postprocessing/postprocessing_tools.R
@@ -69,6 +69,49 @@ gap_fill_na_predictions = function(df, max_gap_size){
     ungroup()
 }
 
+# Helper function for below.
+.assign_sequence_identifier_single_site = function(df){
+  full_date_range = seq(min(df$date), max(df$date), 'day')
+  if(!all(unique(df$date) %in% full_date_range)) stop('Not all dates in df for site: ',unique(df$phenocam_name))
+  
+  df$running_tally = NA
+  df$site_sequence_id = NA
+  present_tally=0
+  seq_id=0
+  for(i in 1:nrow(df)){
+    
+    current_row_present = !is.na(df$`dominant_cover-vegetation`[i])
+    if(current_row_present){
+      present_tally = present_tally + 1
+      missing_tally = 0
+    } else {
+      present_tally = 0
+    }
+    
+    if(present_tally==1){
+      seq_id = seq_id+1
+      df$site_sequence_id[i] = seq_id
+    } else if(present_tally>1){
+      df$site_sequence_id[i] = seq_id
+    }
+    
+    df$running_tally[i] = present_tally
+  }
+  
+  df %>%
+    select(-running_tally)
+}
+
+# For each location assign identifiers to contiguous chunks (ie. non NA) timeseries
+assign_sequence_identifiers = function(df){
+  if(!prediction_df_is_wide(df)) stop('prediction data.frame must be wide format for sequence identification')
+  
+  df %>%
+    group_by(phenocam_name) %>%
+    do(.assign_sequence_identifier_single_site(.)) %>%
+    ungroup()
+  
+}
 
 # Reshaping Tools ----
 # Working with the predictions data.frame requires some back and forth between wide/long formats.
