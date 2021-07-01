95c0d545bbc5a1f24fbc4bc804bbc2300fa36d7c
Shawn <shawntaylor@weecology.org>
2021-07-01 17:35:43 -0600

    decent looking table

--- evaluate_predictions.R
+++ evaluate_predictions.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(zoo)
+library(kableExtra)
 
 source('classification_postprocessing/postprocessing_tools.R')
 source('config.R')
@@ -28,7 +30,9 @@ max_probability = original_predictions %>%
 
 #-------------------------
 # Prep original training data ----
-val_train_split_info = read_csv('./data/val_train_split_info.csv') 
+val_train_split_info = read_csv('./data/val_train_split_info.csv') %>%
+  mutate(data_type = ifelse(is_validation, 'val','train')) %>%
+  select(filename, data_type)
 
 class_descriptions = read_csv('./train_image_annotation/image_classes.csv')
 
@@ -52,7 +56,7 @@ all_training_data = original_training_data %>%
   distinct() %>%   # this distinct drops any duplicates, inevitable with 100k+ images
   pivot_longer(c('crop_status','dominant_cover','crop_type'), names_to='category', values_to='class_value') %>%
   left_join(class_descriptions, by=c('category','class_value')) %>%
-  select(phenocam_name, filename, year, date, category, is_validation, true_class = class_description)
+  select(phenocam_name, filename, year, date, category, data_type, true_class = class_description)
 
 
 #-------------------------
@@ -76,16 +80,69 @@ get_stats = function(d, stats = c('f1','balanced_accuracy')){
 
 all_data = all_training_data %>%
   left_join(select(max_probability,filename,category,maxprob_class), by=c('filename','category'))
+sample_sizes = all_data %>%
+  count(category, true_class) %>%
+  rename(class = true_class, sample_size=n)
 
 prediction_stats = all_data %>%
   rename(predicted_class = maxprob_class) %>%
-  group_by(category, is_validation) %>%
+  group_by(category, data_type) %>%
   nest() %>%
   mutate(stats = map(data, ~get_stats(., stats=c('sensitivity','specificity','f1')))) %>%
   select(-data) %>%
   unnest(stats) %>%
-  pivot_wider(names_from = 'term', values_from='estimate')
+  left_join(sample_sizes, by=c('category','class'))
 
+
+table_row_order = tribble(
+  ~category, ~class, ~rank,
+  'dominant_cover', 'blurry', 1,
+  'dominant_cover', 'vegetation', 2,
+  'dominant_cover', 'residue', 3,
+  'dominant_cover', 'soil', 4,
+  'dominant_cover', 'snow', 5,
+  'dominant_cover', 'water', 6,
+  
+  'crop_type', 'blurry', 7,
+  'crop_type', 'unknown_plant', 8,
+  'crop_type', 'corn', 9,
+  'crop_type', 'wheat/barley', 10,
+  'crop_type', 'soybean', 11,
+  'crop_type', 'alfalfa', 12,
+  'crop_type', 'other', 13,
+  'crop_type', 'no_crop', 14,
+  
+  'crop_status', 'blurry', 15,
+  'crop_status', 'emergence', 16,
+  'crop_status', 'growth', 17,
+  'crop_status', 'flowers', 18,
+  'crop_status', 'senescing', 19,
+  'crop_status', 'senesced', 20,
+  'crop_status', 'no_crop', 21,
+)
+
+# Table formatting stuff happens here
+prediction_stats %>%
+  rename(metric = term, metric_value = estimate) %>%
+  mutate(metric_value = case_when(
+    metric=='sample_size' ~ as.character(metric_value),                     # for sample size keep the same
+    TRUE ~ trimws(format(round(metric_value,2),drop0trailing=F)))) %>% # for decimals keep 2 sig. digis.
+  pivot_wider(names_from = c('metric','data_type'), values_from='metric_value') %>%
+  select(category, class, sample_size, starts_with('sensitivity'), starts_with('specificity'), starts_with('f1'))%>%   # column order of table
+  #mutate(error_text = paste0(val,', ',train)) %>%
+  #select(-train, -val) %>%
+  #pivot_wider(names_from = 'metric', values_from = 'error_text') %>%
+  left_join(table_row_order, by=c('category','class')) %>%
+  arrange(rank) %>%
+  select(-rank) %>%
+  kable(format = 'latex', col.names = c('Category','Class','Sample Size','Training','Validation','Training','Validation','Training','Validation')) %>%
+  add_header_above(c(' '=3, 'Sensitivity' = 2, 'Specificity' = 2, 'F1 Score' = 2))
+
+
+
+#---------------
+# Below here is exploratory stuff
+#---------------
 
 
 # blurry images are exluded from the final hmm model. their original annotation category was just "unknown", which fyi is
